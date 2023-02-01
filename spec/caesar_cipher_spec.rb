dd4e9f90fff09b60a2e5ebfa80b34624bc174534
jacksoze <kamonyojack@gmail.com>
2023-02-01 13:03:42 +0300

    Create a test to test for correct word

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,10 +1,10 @@
-require './lib/caesar_cipher.rb'
+require './lib/caesar_cipher'
 
 describe Caesar_cipher do
-    describe "#caesar_cipher" do
-        it "returns the correct modified string"
-            cipher = Caesar_cipher.new
-            expect(cipher.caesar_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
+  describe '#caesar_cipher' do
+    it 'returns the correct modified string' do
+      cipher = Caesar_cipher.new
+      expect(cipher.caesar_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
     end
+  end
 end
-

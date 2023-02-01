4619315d7015d3095a3d56c587bf0dbcc63b63c8
jacksoze <kamonyojack@gmail.com>
2023-02-01 12:40:53 +0300

    Add the spec folder and the files

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,13 @@
+class Caesar_cipher
+  
+end
+
+
+# caesar_cipher("What a string!!",14)
+
+#created an array of the alphabet
+#split the string to induvidual characters
+#getting character index of the alphabet
+#adjusting the index using the given number
+#pushing the new letter to new wor array
+#joining together the words from the new word array
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,1 +1,10 @@
-require './lib/caesar_cipher'
+require './lib/caesar_cipher.rb'
+
+describe Caesar_cipher do
+    describe "#caesar_cipher" do
+        it "returns the correct modified string"
+            cipher = Caesar_cipher.new
+            expect(cipher.caesar_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
+    end
+end
+
